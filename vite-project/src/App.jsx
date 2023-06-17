b4300d9d80cb8febdf0ac060eb992ac82cc7c9d2
Franciellen <franciellen_gc@hotmail.com>
2023-06-17 14:44:03 +0200

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Simple from './bubbleSimple';
+import Multiple from './bubbleMultiple';
+
+const TestGraph = () => {
+  return (
+    <>
+    <div>
+      <Simple/>
+    </div>
+    <div> 
+      <Multiple/>
+    </div>
+    </>
+  );
+};
+
+export default TestGraph;
+
+//https://www.zingchart.com/docs/chart-types/bubble-pack
